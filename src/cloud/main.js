f2bc8e803894dcc2eddfa63449284f1d06aa48bc
Daniel Andrus <danieleandrus@gmail.com>
2016-01-28 14:50:48 -0700

    Added current code from Parse

--- src/cloud/main.js
+++ src/cloud/main.js
@@ -1,6 +1,29 @@
+Parse.Cloud.define('addTestGroup', function( request, response){
+    var Group = Parse.Object.extend('Group');
+    var relation = Group.relation('Users');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    var query = new Parse.Query('Parse.User');
+    
+
+    query.equalTo('User', ParseUser.current())
+    query.limit(10);
+    query.find({
+        sucess: function(results){
+            if(results.length > 0){
+                for( var i = 0; i < results.length; i++)
+                {
+                    var object = results[i];
+                    Group.addUnique('groupMembers', relation);
+                }
+            }
+            else{
+                responce.error('users not avalable');
+            }
+            group.save();
+            
+        },
+        error: function(error){
+        response.error('and error orrcured');
+    }
+    });
 });
