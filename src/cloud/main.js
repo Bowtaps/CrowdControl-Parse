0dcf7f421a6a9a62679e69a6b6e1098b7522382c
Daniel Andrus <danieleandrus@gmail.com>
2016-01-28 15:47:04 -0700

    WIP: added fetchGroupUpdates function

--- src/cloud/main.js
+++ src/cloud/main.js
@@ -1,3 +1,4 @@
+/*
 Parse.Cloud.define('addTestGroup', function( request, response){
     var Group = Parse.Object.extend('Group');
     var relation = Group.relation('Users');
@@ -26,4 +27,54 @@ Parse.Cloud.define('addTestGroup', function( request, response){
         response.error('and error orrcured');
     }
     });
+});
+*/
+
+/**
+ * Fetches updates to a group since the provided timestamp
+ */
+Parse.Cloud.define('fetchGroupUpdates', function(request, response) {
+	
+	// Verify request parameters
+	var group = request.params.group;
+	var userProfile = request.params.userProfile;
+	var timestamp = request.params.timestamp;
+	
+	// Construct and execute queries
+	var GroupObject = Parse.Object.extend("Group");
+	var UserObject = Parse.Object.extend("CCUser");
+	
+	var resultArray = new Array();
+	
+	// Get group matching given parameters
+	var GroupQuery = new Parse.Query(GroupObject);
+	GroupQuery.get(group).then(function(groupResult) {
+			
+		// If group has seen an update since then, fetch group members
+		if (groupResult.updatedAt < timestamp) {
+			resultArray.push(groupResult);
+
+			// Fetch members in relationship
+			var groupMembersRelation = groupResult.relation("GroupMembers");
+			return groupMembersRelation.query().find();
+		} else {
+			response.success(resultArray);
+		}
+	},
+
+	// Handle errors
+	function(error) {
+		response.error('Unable to match group');
+	}).then(function(memberResults) {
+
+		// Add group members to result array and return success
+		resultArray = resultArray.concat(memberResults);
+		response.success(resultArray);
+	},
+
+	// Handle errors
+	function(error) {
+		response.error('Unable to get group members');
+	});
+	
 });
